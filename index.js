da180754bbbc9e985645dce1ca58be5affbe1151
Hosein <h.haghayeghi@gmail.com>
2021-07-26 11:50:38 +0430

    Implement POST genres

--- index.js
+++ index.js
@@ -22,6 +22,25 @@ app.get('/api/genres/:id',(req,res)=> {
     res.send(genre)
 })
 
+app.post('/api/genres',(req,res)=> {
+    const schema = Joi.object({
+        title : Joi.string().max(15).min(2).required(),
+    })
+    const validattionResults = schema.validate(req.body);
+    
+    if(validattionResults.error) return res.status(400).send(validattionResults.error.details[0].message)
+    else {
+        const genre = {
+            id : genres.length +1,
+            title : validattionResults.value.title,
+        } 
+
+        genres.push(genre)
+        res.send(genres)
+    }
+
+})
+
 app.listen(port,()=>{
     console.log(`service running on port ${port}`);
 })
